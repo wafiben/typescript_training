b6b7c2a2b747a160581ecc012ad491d48ecccd23
wafiben <wafibenjeddou@gmail.com>
2023-05-06 22:27:56 +0200

    before solution

--- file.ts
+++ file.ts
@@ -1,39 +1,82 @@
-//signature of function 
-function add(x: number,y: number): number {
-	return x+y
-}
+// Ce code TypeScript n'est pas complet.
+// Regardez les dernières lignes de ce script : TypeScript remonte des erreurs.
+// Votre but est de modifier le code pour corriger ces erreurs.
+// Vous allez devoir modifier les types déjà définis et en créer d'autres.
+// ====================
 
-///alias
-
-function checkeLife(person: {life: number},amount: number) {
-	person.life-=amount;
-}
-/*===>in this case we can use alias*/
-type Person={life: number|string}
-/*or interface*/
-interface Person {
-	life: number;
-}
-function checkeLife(person: Person,amount: number) {
-	person.life-=amount;
-}
-
-
-type Pet={
+type Character={
 	name: string;
 	life: number;
-	age: number;
+	attack: number;
+	defense: number;
 };
 
-type Hero={
-	name: string;
-	life: number;
-	age: number;
-	pet?: Pet;
-}
-//list or table in type Script 
-type ArrayOfNumber=number[];
-/*or*/
-type ArrayOfNumber=Array<number>
-const x: ArrayOfNumber=[1,2]
+type Pet=Character;
 
+type Hero=Character&{
+	pet?: Pet;
+};
+
+
+// ====================
+// Ne touchez PAS aux lignes ci-dessous, faites en sorte que TypeScript
+// soit content en modifiant UNIQUEMENT le code du dessus !
+
+const babyDragon: Pet={
+	name: 'Typeon',
+	life: 50,
+	attack: 10,
+	defense: 20,
+};
+
+const babyLicorn: MagicalPet={
+	name: 'Typemoor',
+	life: 80,
+	attack: 5,
+	defense: 30,
+	magic: 50,
+};
+
+const smallArmor: Equipment={
+	price: 30,
+	attack: 2,
+	defense: 5,
+};
+
+const smallSword: Equipment={
+	price: 20,
+	attack: 5,
+};
+
+const smallShield: Equipment={
+	price: 15,
+	defense: 10,
+};
+
+const smallMagicalWand: Equipment&WithMagic={
+	price: 50,
+	attack: 4,
+	magic: 10,
+};
+
+const myWarriorHero: Hero={
+	name: 'TSolomon',
+	life: 200,
+	attack: 30,
+	defense: 50,
+	pets: [babyDragon,babyLicorn],
+	equipments: [
+		smallArmor,
+		smallSword,
+		smallShield,
+	],
+};
+
+const myMagicienHero: Magician={
+	name: 'TSoren',
+	life: 150,
+	attack: 20,
+	defense: 40,
+	equipments: [smallMagicalWand],
+	magic: 200,
+};
